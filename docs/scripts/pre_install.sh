3f151179c1747b1722d045341c5bfe28939d499d
Simone Bendazzoli <simben@kth.se>
2025-10-09 10:43:52 +0000

    Update Read the Docs configuration to include PDF and ePub formats, and modify pre_install script to correctly reference session directories for documentation materials.

--- docs/scripts/pre_install.sh
+++ docs/scripts/pre_install.sh
@@ -10,13 +10,13 @@ mkdir -p docs/source/apidocs/Sessions/Day2/ML-framworks
 mkdir -p docs/source/apidocs/Sessions/Day2/Shell-Part-2
 mkdir -p docs/source/apidocs/Sessions/Day2/SLURM-HPC
 mkdir -p docs/source/apidocs/Sessions/Day2/FAQ
-cp -r Day1/MAIA-Intro/* docs/source/apidocs/Sessions/Day1/MAIA-Intro/
-cp -r Day1/Conda/* docs/source/apidocs/Sessions/Day1/Conda/
-cp -r Day1/Git/* docs/source/apidocs/Sessions/Day1/Git/
-cp -r Day1/Shell-Intro/* docs/source/apidocs/Sessions/Day1/Shell-Intro/
-cp -r Day1/SSH_VSCode/* docs/source/apidocs/Sessions/Day1/SSH_VSCode/
-cp -r Day2/ML-framworks/* docs/source/apidocs/Sessions/Day2/ML-framworks/
-cp -r Day2/Shell-Part-2/* docs/source/apidocs/Sessions/Day2/Shell-Part-2/
-cp -r Day2/SLURM-HPC/* docs/source/apidocs/Sessions/Day2/SLURM-HPC/
-cp -r Day2/FAQ/* docs/source/apidocs/Sessions/Day2/FAQ/
+cp -r Sessions/Day1/MAIA-Intro/* docs/source/apidocs/Sessions/Day1/MAIA-Intro/
+cp -r Sessions/Day1/Conda/* docs/source/apidocs/Sessions/Day1/Conda/
+cp -r Sessions/Day1/Git/* docs/source/apidocs/Sessions/Day1/Git/
+cp -r Sessions/Day1/Shell-Intro/* docs/source/apidocs/Sessions/Day1/Shell-Intro/
+cp -r Sessions/Day1/SSH_VSCode/* docs/source/apidocs/Sessions/Day1/SSH_VSCode/
+cp -r Sessions/Day2/ML-framworks/* docs/source/apidocs/Sessions/Day2/ML-framworks/
+cp -r Sessions/Day2/Shell-Part-2/* docs/source/apidocs/Sessions/Day2/Shell-Part-2/
+cp -r Sessions/Day2/SLURM-HPC/* docs/source/apidocs/Sessions/Day2/SLURM-HPC/
+cp -r Sessions/Day2/FAQ/* docs/source/apidocs/Sessions/Day2/FAQ/
 python docs/scripts/generate_docs.py
